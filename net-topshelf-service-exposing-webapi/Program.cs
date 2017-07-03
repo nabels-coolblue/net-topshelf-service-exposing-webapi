068fb64384ee6b63fe5cc18a37c71356155f7f18
Niels Abels <n.abels@coolblue.nl>
2017-07-03 13:20:01 +0200

    Adds Topshelf skeleton project.

--- net-topshelf-service-exposing-webapi/Program.cs
+++ net-topshelf-service-exposing-webapi/Program.cs
@@ -1,8 +1,10 @@
-﻿using System;
+﻿using Serilog;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Topshelf;
 
 namespace net_topshelf_service_exposing_webapi
 {
@@ -10,6 +12,36 @@ namespace net_topshelf_service_exposing_webapi
     {
         static void Main(string[] args)
         {
+            HostFactory.Run(configurator =>
+            {
+                Log.Logger = new LoggerConfiguration()
+                    .ReadFrom.AppSettings()
+                    .WriteTo.ColoredConsole()
+                    .CreateLogger();
+                Log.Logger.Information("Logger configured");
+
+                configurator.UseSerilog();
+                configurator.SetServiceName("Windows.Service.ApiController");
+                configurator.Service<WindowsHttpApiService>(s =>
+                {
+                    s.ConstructUsing(() => new WindowsHttpApiService());
+                    s.WhenStarted(service => service.Start());
+                    s.WhenStopped(service => service.Stop());
+                });
+            });
+        }
+    }
+
+    public class WindowsHttpApiService
+    {
+        public void Start()
+        {
+            Log.Logger.Information("Service started");
+        }
+
+        public void Stop()
+        {
+            Log.Logger.Information("Service stopped");
         }
     }
 }
