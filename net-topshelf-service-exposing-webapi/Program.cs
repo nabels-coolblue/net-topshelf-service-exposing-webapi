5e6b1f50c5b86ec25d55f62ac162d6e841c7b396
Niels Abels <n.abels@coolblue.nl>
2017-07-03 13:53:36 +0200

    Adds ApiController.
    At http://localhost:8080/api/demo.

--- net-topshelf-service-exposing-webapi/Program.cs
+++ net-topshelf-service-exposing-webapi/Program.cs
@@ -1,9 +1,12 @@
-﻿using Serilog;
+﻿using Microsoft.Owin.Hosting;
+using Owin;
+using Serilog;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Web.Http;
 using Topshelf;
 
 namespace net_topshelf_service_exposing_webapi
@@ -32,15 +35,70 @@ namespace net_topshelf_service_exposing_webapi
         }
     }
 
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            var config = new HttpConfiguration();
+            config.Routes.MapHttpRoute(
+                name: "DefaultApi",
+                routeTemplate: "api/{controller}/{id}",
+                defaults: new { id = RouteParameter.Optional }
+            );
+
+            app.UseWebApi(config);
+        }
+    }
+
+    public class DemoController : ApiController
+    {
+        // GET api/demo 
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "Hello", "World" };
+        }
+
+        // GET api/demo/5 
+        public string Get(int id)
+        {
+            return "Hello, World!";
+        }
+
+        // POST api/demo 
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT api/demo/5 
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/demo/5 
+        public void Delete(int id)
+        {
+        }
+    }
+
     public class WindowsHttpApiService
     {
+        IDisposable _WebApp;
+
         public void Start()
         {
+            _WebApp = WebApp.Start<Startup>("http://localhost:8080");
+
+            Console.WriteLine("Web Server is running.");
+            Console.WriteLine("Press any key to quit.");
+            Console.ReadLine();
+            
             Log.Logger.Information("Service started");
         }
 
         public void Stop()
         {
+            _WebApp?.Dispose();
+
             Log.Logger.Information("Service stopped");
         }
     }
